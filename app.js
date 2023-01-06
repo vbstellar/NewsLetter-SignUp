3d6a39124beefb136e088dadfb56b8bf19f5bef3
vishu <bansalvishu56@gmail.com>
2023-01-06 19:16:55 +0530

    Post request accessed

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    var Fname = req.body.fname;
+    var Lname = req.body.lname;
+    var Email = req.body.email;
+
+    console.log(Fname, Lname, Email);
 })
 
 app.listen(3000, ()=>{
